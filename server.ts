74a5d7dea7f75c57f2663c8706bb856a7f25b581
= <=>
2025-08-07 22:35:51 +0500

    final

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
 import express, { Express, Request, Response, NextFunction } from 'express';
-import cors from 'cors';
+import cors from 'cors'; 
 import dotenv from 'dotenv';
 import path from 'path';
 import { connectDB } from './config/database';
